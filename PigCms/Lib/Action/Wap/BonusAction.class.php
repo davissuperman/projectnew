a839a4a76ba7be4abea5be1cf9c7dae2f668cb63
Davis <Davis.Du@sneakerhead.com>
2015-02-02 17:25:26 +0800

    add
    Signed-off-by: Davis <Davis.Du@sneakerhead.com>

--- PigCms/Lib/Action/Wap/BonusAction.class.php
+++ PigCms/Lib/Action/Wap/BonusAction.class.php
@@ -354,8 +354,30 @@ class BonusAction extends Action {
         $this->assign('threelevelid',$threelevelId);
 
         $this->assign('fourtime',date("Y-m-d H:i:s",$fourTime));
+        $this->assign('fourtimediff',$this->timediff($fourTime,time()));
         $this->assign('threetime',date("Y-m-d H:i:s",$threeTime));
+        $this->assign('threetimediff',$this->timediff($threeTime,time()));
         $this->display();
+    }
+    function timediff($begin_time,$end_time)
+    {
+        if($begin_time < $end_time){
+            $starttime = $begin_time;
+            $endtime = $end_time;
+        }
+        else{
+            $starttime = $end_time;
+            $endtime = $begin_time;
+        }
+        $timediff = $endtime-$starttime;
+        $days = intval($timediff/86400);
+        $remain = $timediff%86400;
+        $hours = intval($remain/3600);
+        $remain = $remain%3600;
+        $mins = intval($remain/60);
+        $secs = $remain%60;
+        $res = array("day" => $days,"hour" => $hours,"min" => $mins,"sec" => $secs);
+        return $res;
     }
     public function checkPhone(){
         $phone = $_POST['phone'];
