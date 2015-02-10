305ce35ec914290ef01fe49f338e0984e1a2c037
unknown <yeyuliang@FTQJK-L6095.douyole.com>
2015-02-10 14:29:46 +0800

    change style

--- wap/event/2015/02/year/js/common.js
+++ wap/event/2015/02/year/js/common.js
@@ -161,9 +161,7 @@ function deviceMotionHandler(eventData) {
  function doResult() {	
  	if($("#content-list").css("-webkit-transform") != 'none'){
 		var atrr = $.trim($("#content-list").css("-webkit-transform")).split(",");
-		alert(atrr[1]);
 		if($.trim(atrr[1]) == '0px'){
-			alert("good");
 			var $content_list = $("#content-list");
 			$content_list.find(".p0 .cloud1").addClass("zoomOutUp");
 			$content_list.find(".p0 .cloud4").addClass("zoomOutUp");
