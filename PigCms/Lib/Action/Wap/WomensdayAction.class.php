30d1b4d81c27271206ebedabcfb99db6a4da1244
Davis <Davis.Du@sneakerhead.com>
2015-03-04 11:12:28 +0800

    add
    Signed-off-by: Davis <Davis.Du@sneakerhead.com>

--- PigCms/Lib/Action/Wap/WomensdayAction.class.php
+++ PigCms/Lib/Action/Wap/WomensdayAction.class.php
@@ -641,6 +641,155 @@ class WomensdayAction extends BonusAction {
         $this->display();
     }
 
+    //保存电话和地址信息
+    public function reg(){
+        $agent = $_SERVER['HTTP_USER_AGENT'];
+//        if (!strpos($agent, "MicroMessenger") && !isset($_GET['show'])) {
+//            echo '此功能只能在微信浏览器中使用';
+//            exit;
+//        }
+//         if (time() > $this->gameinfo['end']) {//活动是否结束
+//                exit('<center>游戏已经结束！谢谢你的参与</center>');
+//          }
+        $userOpenId= cookie('user_openid');
+        if(!$userOpenId)
+            $userOpenId = 'localenv';
+
+        $gid = 1;
+        //统计end
+
+        //即使存在与cookie但是fans中不存在必须重新获取
+        $selfUserInfo = array();
+        if ($_GET['show']) {
+            $userOpenId= $_GET['openid'];
+            $fansInfo = M('customer_service_fans')->where(array('openid' => $userOpenId,'token'=>'rggfsk1394161441'))->find();
+
+        } else {
+            //根据open id获取用户信息，查看是否存在
+            $fansInfo = M('customer_service_fans')->where(array('openid' => $userOpenId,'token'=>'rggfsk1394161441'))->find();
+            if($userOpenId && $fansInfo){
+                //如果存在 则不需要再通过weixin API调取用户信息
+                $selfUserInfo['headimgurl'] = $fansInfo['headimgurl'];
+                $selfUserInfo['nickname'] = $fansInfo['nickname'];
+            }else{
+                $apidata = M('Diymen_set')->where(array('token' => 'rggfsk1394161441'))->find(); //这token 写死了
+                $code = trim($_GET["code"]);
+                $state = trim($_GET['state']);
+                if ($code && $state == 'sentian') {
+                    //查看web_acccess_token是否过期
+                    if(empty($fansInfo)){
+                        $webCreatetime = $apidata['web_createtime'];
+                        $web_access_token = '';
+
+                        if($webCreatetime>(time()-7200) && $userOpenId){
+                            //未过期
+                            $web_access_token = $apidata['web_access_token'];
+                        }else if($webCreatetime<=(time()-7200) && $userOpenId && isset($apidata['refresh_token']) && ($apidata['refresh_token_createtime']>(time()-7*3600*24))  ){
+                            //从新获取通过
+                            $urlRefreshToken = 'https://api.weixin.qq.com/sns/oauth2/refresh_token?appid='.
+                                $apidata['appid'].'&grant_type=refresh_token&refresh_token='.$apidata['refresh_token'];
+                            $jsonRefresh = json_decode($this->curlGet($urlRefreshToken));
+                            $web_access_token = $jsonRefresh->access_token;
+                            $refresh_token = $jsonRefresh->refresh_token;
+                            $m['id'] = $apidata['id'];
+                            $m['web_access_token'] = $web_access_token;
+                            $m['refresh_token'] =$refresh_token;
+                            $m['web_createtime'] = time();
+                            $m['refresh_token_createtime'] = time();
+                            M('Diymen_set')->save($m);
+                        }else{
+                            //重新获取
+                            $userinfoFromApi = $this->getUserInfo($code, $apidata['appid'], $apidata['appsecret']);
+                            if(isset($userinfoFromApi['errcode']) && $userinfoFromApi['errcode']){
+                                //code 有错误 需要重定向
+                                $url = $this->url."/index.php?g=Wap&m=womensday&a=index";
+                                header("location:$url");
+                            }
+                            $m['id'] = $apidata['id'];
+                            $m['web_access_token'] = $userinfoFromApi['access_token'];
+                            $m['refresh_token'] = $userinfoFromApi['refresh_token'];
+                            $m['web_createtime'] = time();
+                            $m['refresh_token_createtime'] = time();
+                            M('Diymen_set')->save($m);
+                            $web_access_token = $userinfoFromApi['access_token'];
+                            cookie('user_openid', $userinfoFromApi['openid'], 315360000);
+                            $userOpenId = $userinfoFromApi['openid'];
+                        }
+
+                        //根据access_token 拉到用户基本信息
+                        $gUrl = 'https://api.weixin.qq.com/sns/userinfo?access_token='.$web_access_token.'&openid='.$userOpenId.'&lang=zh_CN';
+                        $json = json_decode($this->curlGet($gUrl));
+
+                        $this->saveUserInfo($json);
+                        $selfUserInfo['headimgurl'] = $json->headimgurl;
+                        $selfUserInfo['nickname'] = $json->nickname;
+                    }
+                } else {
+                    $url = urlencode($this->url."/index.php?g=Wap&m=womensday&a=index");
+                    header("location:https://open.weixin.qq.com/connect/oauth2/authorize?appid=" . $apidata['appid'] . "&redirect_uri=$url&response_type=code&scope=snsapi_userinfo&state=sentian#wechat_redirect");
+                    exit;
+                }
+            }
+        }
+        $apidata = M('Diymen_set')->where(array('token' => 'rggfsk1394161441'))->find(); //这token 写死了
+        $accessToken = $apidata['access_token'];
+        $ticket = $apidata['ticket'];
+        if($apidata['access_token']==null || (time()-1*$apidata['create_time']>7200)){
+            //需要重新获取access_token
+            $json = $this->getAccessToken($apidata['appid'],$apidata['appsecret']);
+            $fdata['id'] = $apidata['id'];
+            $fdata['token'] = 'rggfsk1394161441';
+            $fdata['access_token'] = $json['access_token'];
+            $accessToken = $fdata['access_token'];
+            $fdata['create_time'] = time();
+            M('Diymen_set')->save($fdata);
+        }
+        if($apidata['ticket']==null || (time()-1*$apidata['ticket_time']>7200)){
+            $token = $accessToken;
+            $jsonTicket = $this->getJsTicket($token);
+            if($jsonTicket['errmsg'] == 'ok'){
+                $fdata['id'] = $apidata['id'];
+                $fdata['token'] = 'rggfsk1394161441';
+                $fdata['ticket'] = $jsonTicket['ticket'];
+                $ticket = $fdata['ticket'];
+                $fdata['ticket_time'] = time();
+                M('Diymen_set')->save($fdata);
+            }
+        }
+        $noncestr = "Wm3WZYTPz0wzccnW";
+        $timestamp = time();
+        $url = $this->get_url();;
+//        $url = 'http://mp.weixin.qq.com';
+        $str = 'jsapi_ticket='.$ticket.'&noncestr='.$noncestr.'&timestamp='.$timestamp.'&url='.$url;
+
+        $signature = sha1($str);
+        $this->assign("appid",$apidata['appid']);
+        $this->assign("timestamp",$timestamp);
+        $this->assign("nonceStr",$noncestr);
+        $this->assign("signature",$signature);
+
+
+        $this->assign('title',$selfUserInfo['nickname']."38女人节特别奉献!");
+        $this->assign('bonusdesc','');
+        $img = "http://wx.drjou.cc/tpl/Wap/default/common/womensday/images/logo3.png";
+        $this->assign("shareimage",$img);
+        $this->assign('url',  $url."/index.php?g=Wap&m=Womensday&a=index");
+
+        $this->assign("siteurl",$this->url);
+
+        //$userOpenId
+        //判断OPENID是否存在 在表womensday中
+        $info = M('womensday')->where(array('openid' => $userOpenId))->find();
+        $left = 0;
+        if(!$info){
+            //重定向到首页
+            $url = $this->url."/index.php?g=Wap&m=womensday&a=index";
+            header("location:$url");
+        }
+
+
+        $this->display();
+    }
     public function shareInfo(){
         //只要分享了 额外获取一次机会
         $userOpenId= cookie('user_openid');
