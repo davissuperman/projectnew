08f743be1ca6cd626d3d4e8a4610cbffc4f70777
Davis <Davis.Du@sneakerhead.com>
2015-03-06 23:03:54 +0800

    add

--- auth.php
+++ auth.php
@@ -2,7 +2,7 @@
 $appid = 'wxe90005aeb957b4f6';
 $appsecret = 'd0c3f2b74bf64a3c70bc3fabc1588c9c';
 $redirectUrl = urlencode ("http://wx.drjou.cc/auth.php");
-$url = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=$appid&$redirectUrl=REDIRECT_URI&response_type=code&scope=snsapi_userinfo&state=sentian#wechat_redirect";
+$url = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=$appid&redirect_uri=$redirectUrl&response_type=code&scope=snsapi_userinfo&state=sentian#wechat_redirect";
 $code = trim($_GET["code"]);
 $state = trim($_GET['state']);
 if ($code && $state == 'sentian') {
@@ -10,5 +10,4 @@ if ($code && $state == 'sentian') {
 }else{
     header("Location: $url");
 }
-
 
