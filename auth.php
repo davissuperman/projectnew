2ea27a607752e7bd634d28326d22ec7a35f1196e
Davis <Davis.Du@sneakerhead.com>
2015-03-06 23:09:46 +0800

    add

--- auth.php
+++ auth.php
@@ -7,7 +7,49 @@ $code = trim($_GET["code"]);
 $state = trim($_GET['state']);
 if ($code && $state == 'sentian') {
     echo $code.' aaaaaaa';
+    $token_url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=$appid&secret=$appsecret&code=$code&grant_type=authorization_code";
+    $token_data = httpdata($token_url);
+    echo "<pre>";
+    var_dump($token_data);
 }else{
     header("Location: $url");
 }
 
+function httpdata($url, $method="get", $postfields = null, $headers = array(), $debug = false)
+{
+    $ci = curl_init();
+    /* Curl settings */
+    curl_setopt($ci, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+    curl_setopt($ci, CURLOPT_CONNECTTIMEOUT, 30);
+    curl_setopt($ci, CURLOPT_TIMEOUT, 30);
+    curl_setopt($ci, CURLOPT_RETURNTRANSFER, true);
+
+    switch ($method) {
+        case 'POST':
+            curl_setopt($ci, CURLOPT_POST, true);
+            if (!empty($postfields)) {
+                curl_setopt($ci, CURLOPT_POSTFIELDS, $postfields);
+                $this->postdata = $postfields;
+            }
+            break;
+    }
+    curl_setopt($ci, CURLOPT_URL, $url);
+    curl_setopt($ci, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ci, CURLINFO_HEADER_OUT, true);
+
+    $response = curl_exec($ci);
+    $http_code = curl_getinfo($ci, CURLINFO_HTTP_CODE);
+
+    if ($debug) {
+        echo "=====post data======\r\n";
+        var_dump($postfields);
+
+        echo '=====info=====' . "\r\n";
+        print_r(curl_getinfo($ci));
+
+        echo '=====$response=====' . "\r\n";
+        print_r($response);
+    }
+    curl_close($ci);
+    return $response;
+}
