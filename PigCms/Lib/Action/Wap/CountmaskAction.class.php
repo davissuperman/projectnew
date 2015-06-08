b3f21370333445c07927839cded40c0c125a4840
davis <davis.du@voyageone.cn>
2015-06-08 13:28:19 +0800

    add save info

--- PigCms/Lib/Action/Wap/CountmaskAction.class.php
+++ PigCms/Lib/Action/Wap/CountmaskAction.class.php
@@ -32,12 +32,92 @@ class CountmaskAction extends SjzAction {
         $appId = $apidata['appid'];
         return array($ticket,$appId,$gid);
     }
-    public function index() {
+
+    public function saveInfo($gid,$openId,$nickname,$imageProfile){
+        //首先查看此OPENID 是否存在 无论gid
+        $bonusInfo = M('countmask')->where(array('openid' => $openId))->find();
+        if(!$bonusInfo){
+            //创建个人主页
+            $d['gid'] = $gid;
+            $d['openid'] = $openId;
+            $d['name'] = $nickname;
+            $d['headimgurl'] = $imageProfile;
+            $d['views'] = 1;
+            $d['createtime'] = time();
+            M("countmask")->add($d);
+        }
+    }
+
+
+
+        public function index() {
         $agent = $_SERVER['HTTP_USER_AGENT'];
 //        if (!strpos($agent, "MicroMessenger") && !isset($_GET['show'])) {
 //            echo '此功能只能在微信浏览器中使用';
 //            exit;
 //        }
+        $userOpenId= cookie('user_openid');
+        $fansInfo = null;
+        $selfUserInfo = array();
+        $fansInfo = M('customer_service_fans')->where(array('openid' => $userOpenId,'token'=>'rggfsk1394161441'))->find();
+        if($userOpenId){
+            if($fansInfo){
+                $selfUserInfo['headimgurl'] = $fansInfo['headimgurl'];
+                $selfUserInfo['nickname'] = $fansInfo['nickname'];
+            }
+        }else{
+            $apidata = M('Diymen_set')->where(array('token' => 'rggfsk1394161441'))->find(); //这token 写死了
+            $code = trim($_GET["code"]);
+            $state = trim($_GET['state']);
+            if ($code && $state == 'sentian') {
+                if(empty($fansInfo)){
+                    $webCreatetime = $apidata['web_createtime'];
+                    $web_access_token = '';
+
+                    if($webCreatetime>(time()-7200) && $userOpenId){
+                        //未过期
+                        $web_access_token = $apidata['web_access_token'];
+                    }else{
+                        //重新获取
+                        $userinfoFromApi = $this->getUserInfo($code, $apidata['appid'], $apidata['appsecret']);
+                        if(isset($userinfoFromApi['errcode']) && $userinfoFromApi['errcode']){
+                            //code 有错误 需要重定向
+                            $url = $this->url."/index.php?g=Wap&m=Bonus&a=index&gid=$this->gid";
+                            header("location:$url");
+                        }
+                        $m['id'] = $apidata['id'];
+                        $m['web_access_token'] = $userinfoFromApi['access_token'];
+                        $m['refresh_token'] = $userinfoFromApi['refresh_token'];
+                        $m['web_createtime'] = time();
+                        $m['refresh_token_createtime'] = time();
+                        M('Diymen_set')->save($m);
+                        $web_access_token = $userinfoFromApi['access_token'];
+                        cookie('user_openid', $userinfoFromApi['openid'], 315360000);
+                        $userOpenId = $userinfoFromApi['openid'];
+                    }
+
+                    //根据access_token 拉到用户基本信息
+                    $gUrl = 'https://api.weixin.qq.com/sns/userinfo?access_token='.$web_access_token.'&openid='.$userOpenId.'&lang=zh_CN';
+                    $json = json_decode($this->curlGet($gUrl));
+
+                    $this->saveUserInfo($json);
+                    $selfUserInfo['headimgurl'] = $json->headimgurl;
+                    $selfUserInfo['nickname'] = $json->nickname;
+                }
+            } else {
+                $url = urlencode($this->url."/index.php?g=Wap&m=Countmask&a=index");
+                header("location:https://open.weixin.qq.com/connect/oauth2/authorize?appid=" . $apidata['appid'] . "&redirect_uri=$url&response_type=code&scope=snsapi_userinfo&state=sentian#wechat_redirect");
+                exit;
+            }
+        }
+
+
+
+            $gid = 1;
+            $nickname = $selfUserInfo['nickname'];
+            $imageProfile = $selfUserInfo['headimgurl'];
+        $this->saveInfo($gid,$userOpenId,$nickname,$imageProfile);
+
         list($ticket,$appId,$gid) = $this->getDiymenSet();
         $noncestr = "Wm3WZYTPz0wzccnW";
         $timestamp = time();
@@ -72,5 +152,56 @@ class CountmaskAction extends SjzAction {
     }
     public function rank(){
         $this->display();
+    }
+    /**
+     * 获取用户基本信息并且保存到数据库
+     */
+    public function saveUserInfo($json){
+        $token = "rggfsk1394161441";
+        $fanModel = M('customer_service_fans'); //向粉丝表中添加此用户信息
+        $ffind = $fanModel->where(array('openid' => $json->openid, 'token' => $token))->find();
+
+        if ($ffind) {
+            //$this->subscribe(1); //如果粉丝表中有此用户则重新关注
+        } else {
+            if (property_exists($json, 'errcode') && $json->errcode) {//有些用户没有高级接口，不能获得用户信息
+            } else {
+                $fdata['openid'] =  $json->openid;
+                $fdata['token'] = $token;
+                $fdata['nickname'] = $json->nickname;
+                $fdata['sex'] = $json->sex;
+                $fdata['country'] = $json->country ? $json->country : '未知';
+                if ($fdata['country'] != '中国') {
+                    $fdata['province'] = $fdata['country'];
+                    $fdata['city'] = $json->province ? $json->province : $fdata['province'];
+                } else {
+                    $fdata['province'] = $json->province ? $json->province : $fdata['country'];
+                    $fdata['city'] = $json->city ? $json->city : $fdata['province'];
+                }
+                $fdata['headimgurl'] = $json->headimgurl;
+                $fdata['subscribe_time'] = time();
+                $fdata['subscribe'] = 1;
+                $fdata['u_from'] = 2;
+                $fanModel->add($fdata);
+                //同时吧粉丝资料放到 tp_member_user 表
+                $memberinfo = M('member_user')->where(array('token' => $this->token, 'openid' => $json->openid))->find();
+                $u['u_name'] = $fdata['nickname'];
+                $u['u_sex'] = $fdata['sex'];
+                $u['u_money'] = 0;
+                $u['u_address'] = $fdata['city'];
+                $u['u_member'] = 1;
+                if ($memberinfo) {
+                    M('member_user')->where(array('token' => $token, 'openid' => $json->openid))->save($u);
+                } else {
+                    $u['token'] = $token;
+                    $u['openid'] = $json->openid;
+                    $u['u_form'] = '网页授权';
+                    $u['starttime'] = time();
+                    $u['subscribe'] = 1;
+                    $u['interaction'] = time();
+                    M('member_user')->add($u);
+                }
+            }
+        }
     }
 }
