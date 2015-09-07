c3dc24aac1c4fcc014b51b02f2b55b644319fba2
avis <453432572@qq.com>
2015-09-07 15:51:55 +0800

    add

--- PigCms/Lib/Action/Wap/PrettyAction.class.php
+++ PigCms/Lib/Action/Wap/PrettyAction.class.php
@@ -816,11 +816,37 @@ HTML;
             exit();
         }
         $info = M('pretty')->where(array('openid' => $userOpenId))->find();
+        $gid = $info['gid'];
+        //begin 分享出去的URL
+        list($ticket,$appId,$gidFromDiymenset) = $this->getDiymenSet();
+        $noncestr = "Wm3WZYTPz0wzccnW";
+        $timestamp = time();
+        $url = $this->get_url();;
+        $str = 'jsapi_ticket='.$ticket.'&noncestr='.$noncestr.'&timestamp='.$timestamp.'&url='.$url;
+        $signature = sha1($str);
+        $this->assign("appid",$appId);
+        $this->assign("timestamp",$timestamp);
+        $this->assign("nonceStr",$noncestr);
+        $this->assign("signature",$signature);
+        $this->assign("shareurl",$this->getShareUrl());
+        $this->assign('gid', $gid);
+
+        $this->assign('title',$info['name'].$this->title);
+        $this->assign('bonusdesc',$this->bonusdesc);
+        $this->assign("imageUrl",$this->imageUrl);
+        $this->assign("shareimageurl",$this->shareImageUrl);
+        //end
+
+
         $vote = $info['vote'];
 
         $vote = 16;
         $this->assign("vote",$vote);
         $this->display();
+    }
+
+    public function form(){
+
     }
 
     public function getOrderByOpenId($openId=null){
