104d418eba9384bb77e4d29de5c8c883a06b569a
davissuperman@gmail.com <davissuperman@gmail.com>
2015-11-11 21:46:30 +0800

    add

--- PigCms/Lib/Action/Wap/AnniversaryAction.class.php
+++ PigCms/Lib/Action/Wap/AnniversaryAction.class.php
@@ -8,7 +8,7 @@ class AnniversaryAction extends SjzAction {
     public $shareImageUrl;
     public $endtime="2015-12-20 23:59:59"; //活动结束时间
     public $debug = true; //上线后应该改成false
-    public $defalutGid = 22;
+    public $defalutGid = 29;
     public $anniversaryCount = 25;
 
     public function _initialize() {
