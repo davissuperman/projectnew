932b05fb14c12c56b5dadf29bba30ca288e8f7d4
davissuperman@gmail.com <davissuperman@gmail.com>
2015-12-10 20:34:34 +0800

    add

--- PigCms/Lib/Action/Wap/AnniversaryAction.class.php
+++ PigCms/Lib/Action/Wap/AnniversaryAction.class.php
@@ -1099,7 +1099,7 @@ HTML;
             M('anniversary_phonelist')->add($n);
             $url = 'http://mp.weixin.qq.com/s?__biz=MzA4Mjk5OTYxNQ==&mid=210588191&idx=1&sn=b92ed86b0e48cd73707477f2200d835e&scene=1&srcid=09242PtEGocqLViEACaoNz7Z#rd';
             //redirect
-            header("$url");
+            header("location:$url");
             exit();
         }
         $gid = $_GET['gid'];
