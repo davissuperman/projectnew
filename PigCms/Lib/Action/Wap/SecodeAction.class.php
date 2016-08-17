5313e7842e9e2d3491a301b3d41346b5fff15875
davissuperman@gmail.com <davissuperman@gmail.com>
2016-08-17 16:37:17 +0800

    Add secode action

--- PigCms/Lib/Action/Wap/SecodeAction.class.php
+++ PigCms/Lib/Action/Wap/SecodeAction.class.php
@@ -2,19 +2,21 @@
 require realpath (__ROOT__)."/Lib/Core/Ftp.class.php";
 class SecodeAction extends Action {
     public $fileDate;
+    public $tab;
     public function _initialize() {
         $this->fileDate = date("Ymd",strtotime("-1 day"));
+        $this->tab = '	  ';
     }
     public function generateHyFile(){
         //获取当天的数据
         $start = date("Y-m-d H:i:s",mktime(0,0,0,date("m"),date("d")-1,date("Y")));
         $end = date("Y-m-d H:i:s",mktime(23,59,59,date("m"),date("d")-1,date("Y")));
-        $list = M('customer_service_fans')->query("SELECT * from tp_customer_service_fans order by subscribe_time");
+        $list = M('customer_service_fans')->query("SELECT * from tp_customer_service_fans order by subscribe_time limit 30000");
         $listArr = array();
         $fileData = $this->fileDate;
         $localfile = DATA_PATH.'HY_'.$fileData.'.txt';
-        $firstLine = "微信ID,昵称,性别,国家,省份,城市\n";
-        file_put_contents($localfile,$firstLine);
+//        $firstLine = "微信ID,昵称,性别,国家,省份,城市\n";
+//        file_put_contents($localfile,$firstLine);
         foreach($list as $key => $each){
             $sex = $each['sex'];
             if($sex == 1){
@@ -24,7 +26,7 @@ class SecodeAction extends Action {
             }else{
                 $sex='未知';
             }
-            $eachLine = $each['openid'].",". $each['nickname'].",".$sex.",". $each['country'].",". $each['province'].",". $each['city']."\n";
+            $eachLine = $each['openid'].$this->tab. $each['nickname'].$this->tab.$sex.$this->tab. $each['country'].$this->tab. $each['province'].$this->tab. $each['city']."\n";
             file_put_contents($localfile,$eachLine,FILE_APPEND);
         }
 
@@ -35,21 +37,28 @@ class SecodeAction extends Action {
         $start = date("Y-m-d H:i:s",mktime(0,0,0,date("m"),date("d")-1,date("Y")));
         $end = date("Y-m-d H:i:s",mktime(23,59,59,date("m"),date("d")-1,date("Y")));
         $list = M('Secode')->query(
-            "SELECT * from tp_secode where createtime >= '$start' and createtime<='$end' and status=1
+            "SELECT secode.*,fans.nickname from tp_secode as secode
+              left join tp_customer_service_fans as fans on (fans.openid=secode.openid)
+              where secode.createtime >= '$start' and secode.createtime<='$end' and secode.status=1
                order by createtime desc");
         $listArr = array();
         $fileData = $this->fileDate;
         $localfile = DATA_PATH.'FWCX_'.$fileData.'.txt';
-        $firstLine = "防伪码,微信ID,查询日期,查询IP\n";
-        file_put_contents($localfile,$firstLine);
+//        $firstLine = "防伪码,微信ID,查询日期,查询IP\n";
+//        file_put_contents($localfile,$firstLine);
         foreach($list as $key => $each){
             $tmp = array();
             $tmp['code'] = $each['code'];
             $tmp['openid'] = $each['openid'];
             $tmp['ip'] = $each['ip'];
             $tmp['createtime'] = $each['createtime'];
+            $tmp['nickname'] = $each['nickname'];
             $listArr[] = $tmp;
-            $eachLine = $each['code'].",". $each['openid'].",". $each['createtime'].",".$each['ip']."\n";
+            $ip = $each['ip'];
+            if(!$ip){
+                $ip = 'unknown';
+            }
+            $eachLine = $each['code'].$this->tab. $each['nickname'].$this->tab. $each['createtime'].$this->tab.$ip."\n";
             file_put_contents($localfile,$eachLine,FILE_APPEND);
         }
 
