e6c8fd8e051424cd426a88c032e26c2e56bac900
davissuperman@gmail.com <davissuperman@gmail.com>
2016-09-17 16:08:34 +0800

    add xiezhuangmian

--- PigCms/Lib/Action/Wap/XiezhuangAction.class.php
+++ PigCms/Lib/Action/Wap/XiezhuangAction.class.php
@@ -1276,6 +1276,7 @@ HTML;
                 $p = array();
                 $p['uid'] =  $info['id'];
                 $p['phone'] =  1;
+                $p['createtime'] =  time();
                 $paiming = M('xiezhuang_phonelist')->add($p);
                 $i = array();
                 $i['id'] = $info['id'];
