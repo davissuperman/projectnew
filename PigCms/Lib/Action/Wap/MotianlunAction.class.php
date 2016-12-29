8d376a6a1f4f1fe98c9f0d6355ff7fc83bfa0e17
davissuperman@gmail.com <davissuperman@gmail.com>
2016-12-29 22:35:39 +0800

    add xiezhuangmian

--- PigCms/Lib/Action/Wap/MotianlunAction.class.php
+++ PigCms/Lib/Action/Wap/MotianlunAction.class.php
@@ -43,6 +43,42 @@ class MotianlunAction extends SjzAction {
         }
     }
 
+    public function getInfo(){
+        $userOpenId = null;
+        $apidata = M('Diymen_set')->where(array('token' => 'rggfsk1394161441'))->find(); //这token 写死了
+        $code = trim($_GET["code"]);
+        $state = trim($_GET['state']);
+
+        $fansInfo = M('customer_service_fans')->field('openid,nickname')->where(array('openid' => $userOpenId,'token'=>'rggfsk1394161441'))->find();
+        if ($code && $state == 'sentian') {
+            if(empty($fansInfo)){
+                $webCreatetime = $apidata['web_createtime'];
+                $web_access_token = '';
+
+                //重新获取
+                $userinfoFromApi = $this->getUserInfo($code, $apidata['appid'], $apidata['appsecret']);
+                if(isset($userinfoFromApi['errcode']) && $userinfoFromApi['errcode']){
+                    //code 有错误 需要重定向
+                    $url = $this->url."/index.php?g=Wap&m=Motianlun&a=getInfo";
+                    header("location:$url");
+                }
+                $m['id'] = $apidata['id'];
+                $m['web_access_token'] = $userinfoFromApi['access_token'];
+                $m['refresh_token'] = $userinfoFromApi['refresh_token'];
+                $m['web_createtime'] = time();
+                $m['refresh_token_createtime'] = time();
+                M('Diymen_set')->save($m);
+                $web_access_token = $userinfoFromApi['access_token'];
+                cookie('user_openid', $userinfoFromApi['openid'], 315360000);
+                $userOpenId = $userinfoFromApi['openid'];
+                echo $userOpenId;
+            }
+        } else {
+            $url = urlencode($this->url."/index.php?g=Wap&m=Motianlun&a=getOpenId");
+            header("location:https://open.weixin.qq.com/connect/oauth2/authorize?appid=" . $apidata['appid'] . "&redirect_uri=$url&response_type=code&scope=snsapi_base&state=sentian#wechat_redirect");
+            exit;
+        }
+    }
     public function getShareUrl(){
         $mainId = null;
         $mainGid = null;
