e5cb3895134df4f36d027f26ea007f7ec5ada3fe
davissuperman@gmail.com <davissuperman@gmail.com>
2017-03-02 14:22:07 +0800

    后台

--- PigCms/Lib/Action/User/WomenwheelAction.class.php
+++ PigCms/Lib/Action/User/WomenwheelAction.class.php
@@ -962,8 +962,8 @@ award.address as addres,award.orderid as orderid,award.username as username from
         //每日数据汇总（记录每天活动所有模板所产生的数据总数）
 
         //记录从6.20 到 7.20号每天产生的模板总数
-        $fromDate = strtotime("2016-12-25 00:00:00");
-        $endDate = strtotime("2017-01-20 00:00:00");
+        $fromDate = strtotime("2017-3-2 00:00:00");
+        $endDate = strtotime("2017-3-20 00:00:00");
         $i = 0;
         $datereport = array();
         while($i<35){
@@ -1046,8 +1046,8 @@ award.address as addres,award.orderid as orderid,award.username as username from
         //每日数据汇总（记录每天活动所有模板所产生的数据总数）
 
         //记录从6.20 到 7.20号每天产生的模板总数
-        $fromDate = strtotime("2016-12-25 00:00:00");
-        $endDate = strtotime("2017-01-20 00:00:00");
+        $fromDate = strtotime("2017-3-2 00:00:00");
+        $endDate = strtotime("2017-3-20 00:00:00");
         $i = 0;
         $datereport = array();
         while($i<35){
@@ -1092,15 +1092,15 @@ award.address as addres,award.orderid as orderid,award.username as username from
         set_time_limit(0);
 
         //获取所有模板
-        $query = "select cid as gid,cname as title from tp_npic_twocode where channel_type=6 order by cid asc";
+        $query = "select cid as gid,cname as title from tp_npic_twocode where channel_type=7 order by cid asc";
         $glist = M('bonus')->query($query);
         $this->assign('glist', $glist);
 
         //每日渠道汇总表
         $m = 0;
         $datereport2 = array();
-        $fromDate2 = strtotime("2016-12-25 00:00:00");
-        $endDate2 = strtotime("2017-01-20 00:00:00");
+        $fromDate2 = strtotime("2017-3-1 00:00:00");
+        $endDate2 = strtotime("2017-3-20 00:00:00");
         while($m<35){
             $gidArr = array();
             $add = 24*3600;
@@ -1134,8 +1134,8 @@ award.address as addres,award.orderid as orderid,award.username as username from
         //每日渠道汇总表
         $m = 0;
         $datereport2 = array();
-        $fromDate2 = strtotime("2016-12-25  00:00:00");
-        $endDate2 = strtotime("2017-01-20 00:00:00");
+        $fromDate2 = strtotime("2017-3-1  00:00:00");
+        $endDate2 = strtotime("2017-03-20 00:00:00");
         while($m<35){
             $gidArr = array();
             $add = 24*3600;
@@ -1195,7 +1195,7 @@ award.address as addres,award.orderid as orderid,award.username as username from
             $obj2 = $objPHPExcel->setActiveSheetIndex(0)
                 ->setCellValue('A' . ($n + 2), $n+1)
                 ->setCellValue('B' . ($n + 2), $data[$n]['date']);
-            $query = "select cid as gid,cname as title from tp_npic_twocode where channel_type=5 order by cid asc";
+            $query = "select cid as gid,cname as title from tp_npic_twocode where channel_type=7 order by cid asc";
             $glist = M('bonus')->query($query);
             foreach($glist as $key => $each){
                 $obj2 = $obj2->setCellValue($arrayKeyList2[$key+2] . ($n + 2), $data[$n][$each['gid']]);
